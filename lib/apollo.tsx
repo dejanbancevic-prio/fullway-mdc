3cabc1f1573e360e687a496b9793f5394f8d39d6
Dejan <dejan.bancevic@prioritytire.com>
2025-09-18 15:34:17 +0200

    v0.4.1

--- lib/apollo.tsx
+++ lib/apollo.tsx
@@ -3,7 +3,7 @@ import fetch from "cross-fetch";
 
 export const apolloClient = new ApolloClient({
   link: new HttpLink({
-    uri: process.env.NEXT_PUBLIC_API_URL,
+    uri: process.env.NEXT_API_URL,
     fetch,
   }),
   cache: new InMemoryCache(),
