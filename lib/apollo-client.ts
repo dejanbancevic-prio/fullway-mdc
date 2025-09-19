e3d4bf7f858927837cb711d8aa4ee948b587a997
Dejan <dejan.bancevic@prioritytire.com>
2025-09-19 08:10:55 +0200

    v0.4.4

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -2,11 +2,17 @@ import { ApolloClient, InMemoryCache, HttpLink, makeVar } from "@apollo/client";
 import fetch from "cross-fetch";
 
 export function createApolloClient() {
+  const uri =
+    typeof window === "undefined"
+      ? process.env.NEXT_API_URL
+      : "/api/graphql"; 
+
   return new ApolloClient({
     link: new HttpLink({
-      uri: process.env.NEXT_API_URL,
+      uri,
       fetch,
     }),
     cache: new InMemoryCache(),
   });
 }
+
