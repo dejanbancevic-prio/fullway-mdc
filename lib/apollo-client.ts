5a9fbe85798b09ae412322a8098fe7551dc3bd6f
Dejan <dejan.bancevic@prioritytire.com>
2025-09-19 08:23:33 +0200

    v0.4.6

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink, makeVar } from "@apollo/client";
 import fetch from "cross-fetch";
 
 export function createApolloClient() {
-  if (typeof window === "undefined") {
+  if (typeof window !== "undefined") {
     console.log("SSR Apollo URI ddadadadadadadada:", process.env.NEXT_API_URL);
   }
 
