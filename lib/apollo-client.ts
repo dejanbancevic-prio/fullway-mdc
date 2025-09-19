8605864ffd35237effeb2364314b4ff3bc38a8f3
Dejan <dejan.bancevic@prioritytire.com>
2025-09-19 08:53:49 +0200

    v0.4.93

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -1,20 +1,12 @@
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
-import fetch from "cross-fetch";
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import fetch from 'cross-fetch';
 
 export function createApolloClient() {
-  const isServer = typeof window === "undefined";
-
-  if (isServer) {
-    console.log("SSR Apollo - server ENV NEXT_API_URL:", process.env.NEXT_API_URL);
-  } else {
-    console.log("Apollo running client-side");
-  }
+  const isServer = typeof window === 'undefined';
 
   const uri = isServer
-    ? (process.env.NEXT_PUBLIC_SITE_URL ? `${process.env.NEXT_PUBLIC_SITE_URL}/api/graphql` : "http://localhost:3000/api/graphql")
-    : "/api/graphql";
-
-  console.log("Apollo client using URI:", uri);
+    ? `${process.env.NEXT_PUBLIC_SITE_URL ?? 'http://localhost:3000'}/api/graphql`
+    : '/api/graphql';
 
   return new ApolloClient({
     link: new HttpLink({ uri, fetch }),
