22bb490274e4e1d0a6417b298724225f5d120187
Dejan <dejan.bancevic@prioritytire.com>
2025-09-24 10:58:26 +0200

    v0.6.2 mail test

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -2,23 +2,23 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend();
+//const resend = new Resend();
 
 export async function POST(req: Request) {
   try {
     const { email, firstName, lastName, subject, country, message } = await req.json();
 
-    await resend.emails.send({
-      from: "Website Contact <onboarding@resend.dev>",
-      to: "dejan.bancevic@prioritytire.com",
-      subject: subject || "New Contact Message",
-      text: `
-        From: ${firstName} ${lastName}
-        Email: ${email}
-        Country: ${country}
-        Message: ${message}
-      `,
-    });
+    // await resend.emails.send({
+    //   from: "Website Contact <onboarding@resend.dev>",
+    //   to: "dejan.bancevic@prioritytire.com",
+    //   subject: subject || "New Contact Message",
+    //   text: `
+    //     From: ${firstName} ${lastName}
+    //     Email: ${email}
+    //     Country: ${country}
+    //     Message: ${message}
+    //   `,
+    // });
 
     return NextResponse.json({ success: true });
   } catch (err) {
